956949c31f232d61e3dc4e8e20edaf48b9e826ed
David <fzyentropy@gmail.com>
2023-11-17 13:25:18 -0500

    manage Script folder, add 3 main body part to JSON
    re-organize the script folder, create the "Manager" folder. add objects of 3 main body parts (only add description for "physical body", and no images)

--- Lord of Mystery/Assets/Scripts/Prototype_EarlyGame_Script.cs
+++ Lord of Mystery/Assets/Scripts/Prototype_EarlyGame_Script.cs
@@ -9,6 +9,7 @@ public class Prototype_EarlyGame_Script : MonoBehaviour
     {
         GameManager.GM.Generate_Message("Fortune_Teller_In_Loen");
         GameManager.GM.Generate_Card_Location("The_Fortune_Teller");
+        GameManager.GM.Generate_Card_Body_Part("Physical_Body");
     }
     
     
